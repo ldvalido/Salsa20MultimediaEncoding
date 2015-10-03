faa709e0d1c63e45e31483b3613c972d86943622
lvalido <lvalido@mailexternos.com.ar>
2015-10-03 19:08:19 -0300

    [Salsa20] Implement a basic rtsp server

---
+++ Salsa20.Stream.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Salsa20.Stream.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var server = new StreamServer();
+            while (true)
+            {
+                server.Start();
+            }
+            
+        }
+    }
+}
---
+++ Salsa20.Stream/StreamServer.cs
@@ -0,0 +1,32 @@
+﻿using Media.Rtsp.Server.MediaTypes;
+
+namespace Salsa20.Stream
+{
+    public class StreamServer
+    {
+        public void Start()
+        {
+            //Create the server optionally specifying the port to listen on
+            using (var server = new Media.Rtsp.RtspServer())
+            {
+
+                //Create a stream which will be exposed under the name Uri rtsp://localhost/live/RtspSourceTest
+                //From the RtspSource rtsp://1.2.3.4/mpeg4/media.amp
+
+                //var source = new RtspSource("RtspSourceTest", "rtsp://1.2.3.4/mpeg4/media.amp");
+                var source = new RtspSource("RtspSourceTest", "rtsp://1.2.3.4/mpeg4/media.amp");
+
+                //If the stream had a username and password
+                //source.Client.Credential = new System.Net.NetworkCredential("user", "password");
+
+                //If you wanted to password protect the stream
+                //source.RtspCredential = new System.Net.NetworkCredential("username", "password");
+                server.TryAddMedia(source);
+                //Add the stream to the server
+                
+                //Start the server and underlying streams
+                server.Start();
+            }
+        }
+    }
+}
