dce0f179ad1b42db3f6412c21b470eb693163783
lvalido <lvalido@mailexternos.com.ar>
2015-11-25 19:46:28 -0300

    Remove Unnecesary dependences

--- Salsa20.Stream.Console/Program.cs
+++ Salsa20.Stream.Console/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography;
 using System.Text;
 using Salsa20.Stream.Console.Commands;
 using Salsa20.Stream.Console.ImageFormat;
